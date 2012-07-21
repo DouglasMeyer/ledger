e6cc651081e037cc31ea3c265cd755ed8076d31c
Douglas Meyer <Douglas.Meyer@Mail.com>
2012-07-21 09:55:27 -0500

    BankEntries is now a paginated list.

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -1,6 +1,24 @@
 var BankEntriesCollection = Backbone.Collection.extend({
   model: BankEntry,
-  url: '/bank_entries'
+  url: '/bank_entries',
+  fetch: function(options){
+    options = options ? _.clone(options) : {};
+    if (options.parse === undefined) options.parse = true;
+    var collection = this;
+    var success = options.success;
+    options.success = function(resp, status, xhr) {
+      collection.more = xhr.getResponseHeader('X-More');
+      collection[options.add ? 'add' : 'reset'](collection.parse(resp, xhr), options);
+      if (success) success(collection, resp);
+    };
+    options.error = Backbone.wrapError(options.error, collection, options);
+    return (this.sync || Backbone.sync).call(this, 'read', this, options);
+  },
+  loadMore: function(options){
+    options.url = this.more;
+    options.add = true;
+    this.fetch(options);
+  }
 });
 
 var AccountsCollection = Backbone.Collection.extend({
--- app/controllers/bank_entries_controller.rb
+++ app/controllers/bank_entries_controller.rb
@@ -1,9 +1,16 @@
 class BankEntriesController < ApplicationController
   def index
-    @bank_entries = BankEntry.scoped
+    @bank_entries = BankEntry.order("date DESC, id DESC")
     respond_to do |format|
       format.html
-      format.json { render :json => @bank_entries }
+      format.json do
+        @bank_entries = @bank_entries.limit(24)
+        page = params[:page].to_i || 0
+        if @bank_entries.offset(24 * (page+1)).any?
+          response.headers['X-More'] = bank_entries_url(:page => page+1)
+        end
+        render :json => @bank_entries.offset(24 * page)
+      end
     end
   end
 
