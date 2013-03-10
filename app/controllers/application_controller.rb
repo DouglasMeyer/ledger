a95aa921c075e3f85720dec75c0aa97fb01bd1b6
Douglas Meyer <Douglas.Meyer@Mail.com>
2013-03-10 07:45:24 -0500

    Allow for http authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+private
+  def authenticate
+    if (name = ENV['AUTH_NAME']) && (pass = ENV['AUTH_PASSWORD'])
+      unless authenticate_with_http_basic { |u, p| u == name && p = pass }
+        request_http_basic_authentication
+      end
+    end
+  end
+
 end
