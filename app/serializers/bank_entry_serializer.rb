86226dc84447482f83915c31ac3c6097b69d6cfa
Douglas Meyer <Douglas.Meyer@mail.com>
2014-10-06 23:04:06 -0500

    Updated serializer syntax.

--- app/serializers/bank_entry_serializer.rb
+++ app/serializers/bank_entry_serializer.rb
@@ -1,7 +1,23 @@
 class BankEntrySerializer < ActiveModel::Serializer
   attributes :id, :date, :notes, :description,
-    ammountCents: :ammount_cents,
-    externalId: :external_id,
-    createdAt: :created_at,
-    updatedAt: :updated_at
+    :amountCents,
+    :externalId,
+    :createdAt,
+    :updatedAt
+
+  def amountCents
+    object.amount_cents
+  end
+
+  def externalId
+    object.external_id
+  end
+
+  def createdAt
+    object.created_at
+  end
+
+  def updatedAt
+    object.updated_at
+  end
 end
