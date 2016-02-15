f625bceb0158b6f1f4e872333fd0c161989647da
Douglas Meyer <Douglas.Meyer@mail.com>
2016-02-14 20:27:19 -0600

    Disable the manifest unless you are on a page you want to cache.
    Apparently, specifying a cache_manifest on a page will implicitly cache
    that page.

--- app/controllers/v3/pages_controller.rb
+++ app/controllers/v3/pages_controller.rb
@@ -1,6 +1,7 @@
 module V3
   class PagesController < BaseController
     def angular
+      @do_manifest = Rails.env.production?
       render text: '<div ng-view></div>', layout: true
     end
   end
