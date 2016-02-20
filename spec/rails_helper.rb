4b148aae9bf38eae909a867b40c5018355babb0c
Douglas Meyer <Douglas.Meyer@mail.com>
2016-02-20 17:01:57 -0600

    Updating every gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 Capybara.configure do |config|
-  config.default_wait_time = 5
+  config.default_max_wait_time = 5
   config.default_driver = :webkit
   config.ignore_hidden_elements = false
 end
--- spec/support/pages/base_page.rb
+++ spec/support/pages/base_page.rb
@@ -6,7 +6,7 @@ class BasePage < SitePrism::Page
   section :navigation, NavigationSection, '.navigation'
 
   def page_action(title)
-    Capybara.using_wait_time Capybara.default_wait_time do
+    Capybara.using_wait_time Capybara.default_max_wait_time do
       element_exists?(%Q|.page-actions > span[title="#{title}"]|)
     end
     find(%Q|.page-actions > span[title="#{title}"]|)
