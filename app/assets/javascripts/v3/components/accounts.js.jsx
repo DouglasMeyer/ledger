967decdce92246e98e7d40e56e7a9140af5e1246
Douglas Meyer <Douglas.Meyer@mail.com>
2016-02-25 08:32:09 -0600

    Some clean-up for Accounts componenet.
    Also got formatting currency working.

--- app/assets/javascripts/v3/components/accounts.js.jsx
+++ app/assets/javascripts/v3/components/accounts.js.jsx
@@ -12,6 +12,49 @@ angular.module('ledger').value('AccountsComponent', React.createClass({
   }
 }));
 
+function formatCurrency(cents){
+  dollars = cents / 100;
+  return dollars.toLocaleString('en-US', {style:'currency', currency: 'USD'});
+}
+
+function Account(props){
+  var account = props.account;
+  return (
+    <div key={ account.id } className="m-line" is l-flex="between">
+      <a href={ "/v3/accounts/" + account.id }>{ account.name }</a>
+      <span is class={ 'm-balance' + (account.balance_cents < 0 ? ' is-negative' : '') } l-width="5" l-margin="r1" t-align="right">
+        { formatCurrency(account.balance_cents) }
+      </span>
+    </div>
+  );
+}
+
+function Category(props){
+  var accounts = props.accounts,
+      accountViews = accounts
+        .map(a=>a.category)
+        .filter((category,index,self)=>self.indexOf(category) === index)
+        .map(category => {
+          const accountViews = accounts
+            .filter(a => a.category == category)
+            .map(account => <Account account={account} />);
+
+          return (
+            <div key={ category } className="m-category">
+              <h4 className="m-line" is l-flex>{ category }</h4>
+              { accountViews }
+            </div>
+          );
+        });
+
+  return (
+    <div>
+      <h2 is t-align="center">{ props.title }</h2>
+      { accountViews }
+    </div>
+  );
+}
+
 var Accounts = ReactRedux.connect(s=>s)(React.createClass({
   displayName: 'Accounts',
 
@@ -22,32 +65,6 @@ var Accounts = ReactRedux.connect(s=>s)(React.createClass({
   componentDidMount: function(){
     const { dispatch } = this.props;
     dispatch(actions.ensureAccounts());
-  },
-
-  renderCategories: function(accounts){
-    return accounts
-      .map(a=>a.category)
-      .filter((category,index,self)=>self.indexOf(category) === index)
-      .map(category => {
-        const accountViews = accounts
-          .filter(a => a.category == category)
-          .map(account => {
-            return (
-              <div key={ account.id } className="m-line" is l-flex="between">
-                <a href={ "/v3/accounts/" + account.id }>{ account.name }</a>
-                <span className="m-balance" is l-width="5" l-margin="r1" t-align="right">
-                  { account.balance_cents }
-                </span>
-              </div>
-            );
-          });
-        return (
-          <div key={ category } className="m-category">
-            <h4 className="m-line" is l-flex>{ category }</h4>
-            { accountViews }
-          </div>
-        );
-      });
   },
 
   render: function(){
@@ -61,14 +78,8 @@ var Accounts = ReactRedux.connect(s=>s)(React.createClass({
         .sort((a,b)=> a.position - b.position);
     return (
       <div is l-flex="wrap center" l-margin="b3">
-        <div is l-margin="r2">
-          <h2 is t-align="center">Asset</h2>
-          { this.renderCategories(assetAccounts) }
-        </div>
-        <div>
-          <h2 is t-align="center">Liability</h2>
-          { this.renderCategories(liabilityAccounts) }
-        </div>
+        <Category accounts={assetAccounts} title="Asset" is l-margin="r2" />
+        <Category accounts={liabilityAccounts} title="Liability" />
       </div>
     );
   }
